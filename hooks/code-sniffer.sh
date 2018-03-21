90305e3e0847db650fa19935be2862ef25740def
dakku <dakku@97634.no-reply.drupal.org>
2018-03-21 23:40:17 +0000

    Add Drupal practice phpcs

--- hooks/code-sniffer.sh
+++ hooks/code-sniffer.sh
@@ -12,5 +12,7 @@ vendor/bin/phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer
 
 vendor/bin/phpmd modules/$1 html cleancode,codesize,design,unusedcode --ignore-violations-on-exit --reportfile artifacts/phpmd/index.html
 vendor/bin/phpmetrics --extensions=php,inc,module --report-html=artifacts/phpmetrics --git modules/$1
+
 # Check coding standards
 vendor/bin/phpcs --standard=Drupal --report=junit --report-junit=artifacts/phpcs/phpcs.xml modules/$1
+vendor/bin/phpcs --standard=DrupalPractice --report=junit --report-junit=artifacts/phpcs/phpcs.xml modules/$1
